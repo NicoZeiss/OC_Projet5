596d4f16970ccaddd3abbdf5e3191b4cb8f9b8c3
nicolas <zeissern@gmail.com>
2019-06-05 17:27:51 +0200

    Add constants.py file

---
+++ constants.py
@@ -0,0 +1,8 @@
+# Here are all the constants we'll use in our project
+
+
+# URLs
+api_url = 'https://fr.openfoodfacts.org/api/v0/product/'
+product_url = 'https://fr.openfoodfacts.org/produit/'
+
+# Categories
