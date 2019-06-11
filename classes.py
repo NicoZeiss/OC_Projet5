62f4bb6230eec537f411db199891ad19ea17fc46
nicolas <zeissern@gmail.com>
2019-06-11 03:57:38 +0200

    Create 3 tables in database

--- classes.py
+++ classes.py
@@ -1,10 +1,13 @@
 import requests
+import mysql.connector
 from constants import api_url, product_url
+from IDs import username, pw
 
 
 class Product(object):
 
     def __init__(self, barcode, cat):
+        self.code = barcode
         self.api_url = api_url + barcode +'.json'
         self.url = product_url + barcode
         self.category = cat
@@ -12,7 +15,7 @@ class Product(object):
     def create_prod(self):
         r = requests.get(self.api_url).json()
         product = {"code": "", "name": "", "store": "", "grade": "", "product_url": "", "category": ""}
-        product['code'] = r['code']
+        product['code'] = self.code
         product['name'] = r['product']['product_name']
         product['store'] = r['product']['stores']
         product['grade'] = r['product']['nutrition_grade_fr']
@@ -20,6 +23,15 @@ class Product(object):
         product['category'] = self.category
 
     def save_prod(self):
+        mydb = mysql.connector.connect(
+            host = "localhost",
+            user = username,
+            passwd = pw,
+            database = "projet5")
+        mycursor = mydb.cursor()
+        sql = "INSERT INTO Products"
+
+
         
 
 
