9b741446a84f7e4b5ef517079cc0c902097a107f
nicolas <zeissern@gmail.com>
2019-06-24 15:46:27 +0200

    add some informations about different functions

--- functions.py
+++ functions.py
@@ -9,7 +9,8 @@ Here are all the main functions used into our software
 '''
 
 def update_database(mycursor, mydb):
-    # we update the database
+    '''We update the database'''
+
     create_db(mycursor)
     create_tables(mycursor)
     create_categories(mycursor, mydb)
@@ -22,7 +23,10 @@ def update_database(mycursor, mydb):
 
 
 def cat_choice(mycursor):
+    '''Display category list'''
+
 	cat_choice = "\n"
+    #sql request
 	results = show_cat(mycursor)
 	for result in results:
 		cat_choice += (str(result[0]) + ": " + result[1] + "\n")
@@ -30,6 +34,8 @@ def cat_choice(mycursor):
 	return cat_choice, results[-1][0]
 
 def cat_loop(mycursor, mydb):
+    '''While user has not chose a category with a valid input, the loop is true'''
+
     choice, nb_cat = cat_choice(mycursor)
     while True:
         user_cat = input(choice)
@@ -46,6 +52,9 @@ def cat_loop(mycursor, mydb):
 
 
 def prod_choice(mycursor, user_answer):
+    '''Display product list from the category chosen'''
+
+    #sql request
     results = show_prod(mycursor, user_answer)
     prod_choice = ""
     i = 1
@@ -56,6 +65,8 @@ def prod_choice(mycursor, user_answer):
     return prod_choice, i - 1
 
 def prod_loop(mycursor, user_cat, mydb):
+    '''While user has not chose a product with a valid input, the loop is true'''
+
     choice, nb_prod = prod_choice(mycursor, user_cat)
     while True:
         user_prod = input(choice)
@@ -74,13 +85,19 @@ def prod_loop(mycursor, user_cat, mydb):
 
 
 def show_product(mycursor, user_cat, user_prod):
+    '''We formate the chosen product, and return the result'''
+
     prod_id = (int(user_cat) - 1) * cat_size + int(user_prod)
+    #sql request
     result = select_prod(mycursor, prod_id)
     prod_grade = result[1]
     product = formate_item(result)
     return product, prod_grade, prod_id
 
 def show_substitute(mycursor, user_cat):
+    '''We formate the chosen substitute, and return the result'''
+
+    #sql request
     result = find_substitute(mycursor, user_cat)
     subs_grade = result[1]
     subs_id = result[4]
@@ -88,15 +105,20 @@ def show_substitute(mycursor, user_cat):
     return substitute, subs_grade, subs_id
 
 def formate_item(result):
+    '''Allows us to formate chosen products'''
+
     text = "----------\nNom : {}\nNutriscore : {}\nMagasin : {}\nLien internet : {}\n----------\n"
     formated_text = text.format(result[0], result[1], result[2], result[3])
     return formated_text
 
 def show_result(mycursor, user_cat, user_prod):
+    '''We display chosen product and substitute in the console'''
+
     category = cat_name(mycursor, user_cat)
     show_cat = "\nCatégorie : {}\n".format(category[0])
     product, prod_grade, prod_id = show_product(mycursor, user_cat, user_prod)
     substitute, subs_grade, subs_id = show_substitute(mycursor, user_cat)
+    #Text is different if chosen product has not better substitute
     if prod_grade != subs_grade:
         result = show_cat + template.format(product, substitute)
         subs = True
@@ -114,6 +136,8 @@ def show_result(mycursor, user_cat, user_prod):
 
 
 def save_subs_loop(mycursor, subs, subs_id, prod_id, mydb):
+    '''User has to chose if he wants to saved the result as favorite'''
+
     while True:
         user_answer = input("Désirez-vous ajouter ce produit à vos favoris ? [o/n]\n")
 
@@ -134,6 +158,9 @@ def save_subs_loop(mycursor, subs, subs_id, prod_id, mydb):
 #_________________________________________________
 
 def favorite_list(mycursor):
+    '''We display the favorite list'''
+
+    #sql request
     results = show_subs_list(mycursor)
     subs_list = "\n"
     i = 1
@@ -144,12 +171,16 @@ def favorite_list(mycursor):
     return subs_list, i - 1
 
 def display_result(favorite, original_prod):
+    '''We formate chosen favorite and display the result in our console'''
+
     formated_fav = formate_item(favorite)
     formated_prod = formate_item(original_prod)
     print(("\nNotre suggestion :\n{}\nVotre choix initial :\n{}\n").format(formated_fav, formated_prod))
 
 
 def favorite_loop(mycursor):
+    '''User has to which favorite he wants to read'''
+
     subs_list, nb_subs = favorite_list(mycursor)
     while True:
         user_answer = input(subs_list)
